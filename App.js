03b1848b5c812610119e937ab727f9a0bcdc8e36
Cankat Örüm <52240938+cankat-orum@users.noreply.github.com>
2021-08-17 23:03:37 +0300

    Add Item
    - Frontend / styling
    - useState hook
    - Adding items to an array with rhe seperator operator ( ... )
    - Text event handling
    - display array with mapping
    - Use Flatlist instead of ScrollView to display arrays
    - Giving each array list item a unique key

--- App.js
+++ App.js
@@ -1,23 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+  FlatList,
+} from "react-native";
 
 export default function App() {
-  const [text, setText] = useState('Open up App.js to start working on your app!');
+  const [item, setItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  const inputHandler = (text) => {
+    setItem(text);
+  };
+
+  const addItemHandler = () => {
+    setItems((currentItems) => [
+      ...currentItems,
+      { key: Math.random().toString(), value: item },
+    ]); //currentItem is for RN to always use the latest state snapshot / key gives FlatList unique keys / item is now an object
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>{text}</Text>
-      <Button title="Change Text" onPress={() => setText('TEXT CHANGED!')} />
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.inputCon}>
+        <TextInput
+          placeholder="Item"
+          style={styles.textInput}
+          onChangeText={inputHandler}
+          value={item}
+        />
+        <Button title="AdDd" onPress={addItemHandler} />
+      </View>
+      <FlatList
+        data={items}
+        renderItem={(itemData) => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50,
+  },
+  inputCon: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  textInput: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1,
   },
 });
